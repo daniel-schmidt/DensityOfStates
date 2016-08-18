d72e2942ff7ea91e4a50beaf565b8796b16936a0
Daniel Schmidt <d.schmidt@uni-jena.de>
2016-08-18 17:21:19 +0200

    Imported Submodule FermiOwnCommon, simple executable.

---
+++ apps/WangLandauIsing.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : WangLandauIsing.cpp
+// Author      : Daniel Schmidt
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+int main() {
+	std::cout << "Welcome to my own world!" << std::endl; // prints Welcome to my own world!
+	return 0;
+}
